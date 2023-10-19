26506b248cb86761845c5b79f1395269f8b26a39
sabbir1791 <sabbirh8181@gmail.com>
2023-10-19 15:18:49 +0600

    reading database product single objectID

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 
@@ -38,6 +38,13 @@ async function run() {
         const cursor = productCollection.find();
         const result = await cursor.toArray();
         res.send(result);
+    })
+
+    app.get('/product/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await productCollection.findOne(query);
+      res.send(result);
     })
 
     app.post('/product', async(req, res) =>{
